4c45e1c420f03e275aa43e57204fdb1c55dabe5a
Oloyede <oloyedebifolu@gmail.com>
2018-04-16 20:03:39 +0100

    files from figma

---
+++ currentdate.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>HNG STAGE 1</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body class="body">
+<div class="container">
+
+<div class="row">
+<div class="col-lg-2"></div>
+<div class="col-lg-8">
+<h2>
+This is my first time using figma and it looks quite useful
+</h2>
+</div>
+
+</div>
+
+<div class="row">
+<div class="col-lg-2"></div>
+<div class="col-lg-8">
+<h3 class="subtext">
+The time on my P.C right now is <?php echo date("D M d, Y G:i a"); ?> 
+</h3>
+</div>
+</div>
+
+</div>
+</body>
+
+</html>
